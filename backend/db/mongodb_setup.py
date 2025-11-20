06d5a792255c1b560aada2813a1a9f3c57010b63
esztertolm <eszter.tolmacsi@gmail.com>
2025-11-20 22:03:33 +0100

    added more log

--- backend/db/mongodb_setup.py
+++ backend/db/mongodb_setup.py
@@ -8,6 +8,7 @@ MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME", "mi_5_db")
 
 if not MONGO_URI:
+    logger.error("MONGO_URI is missing from .env file")
     raise ValueError("MONGO_URI is missing from .env file")
 
 client = MongoClient(MONGO_URI)
